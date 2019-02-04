4cb606137e2a60c9872bd09d2ee7e7026e88aa3b
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-04 19:31:06 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in spellwork_cli.gemspec
+gemspec
---
+++ lib/spellwork_cli.rb
@@ -0,0 +1,6 @@
+require "spellwork_cli/version"
+
+module SpellworkCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
