c2ac25a43e818da0357e5f53e95114a7a4c4bf7b
github username <github email address>
2019-02-05 15:58:16 -0600

    working index scraper

--- lib/spellwork_cli/scraper.rb
+++ lib/spellwork_cli/scraper.rb
@@ -1,10 +1,38 @@
 class SpellworkCli::Scraper
 
   def self.scrape_fandom_wiki_index
-    html = open('https://harrypotter.fandom.com/wiki/List_of_spells#A')
+    html = open('https://harrypotter.fandom.com/wiki/List_of_spells')
     doc = Nokogiri::HTML(html)
 
-    binding.pry
+
+    doc.css('h3 .mw-headline').each.with_index do |element, i|
+      array = []
+      hash = {}
+
+
+      name = element.text
+      #=> "Accio (Summoning Charm)"
+      url = "https://harrypotter.fandom.com" + element.css('a').attr('href').text
+      #=> "/wiki/Summoning_Charm"
+      doc.css('h3 + dl')[i].css('dd').each do |list_item|
+        array << list_item.text
+      end
+      array.each do|item|
+        hash[item.strip.split(": ")[0]] = item.strip.split(": ")[1]
+      end
+      type = hash["Type"]
+      description = hash["Description"]
+
+      binding.pry
+
+    end
+    # name = doc.css('h3 .mw-headline')[1].text
+    # url = doc.css('h3 .mw-headline')[1].css('i a').attr('href').text
+    # doc.css('h3 + dl').first.css('dd').first.text
+      # => "Type: Charm\n"
+      # => "Pronunciation: Ah-bare-toh\n"
+      # => "Description: A spell presumably used to open objects such as doors or windows. \n"
+
 
     # monster_elements = doc.css('ul.colum li ul li a;')
 
